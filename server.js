5a930f954be7393de2fa1b9de93ccfcfa5f2fc08
amailtoamal <amalchacko95@gmail.com>
2018-08-21 00:04:32 +0530

    2

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app = express(),
-    morgan = require('morgan');
-
-Object.assign = require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
 
-app.listen(port, ip);
+
+    app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app;
