a3bbd5caa72081559917f0da5e20e9f4c700aadf
amailtoamal <amalchacko95@gmail.com>
2018-08-21 01:18:08 +0530

    4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 
 //create a server object:
@@ -8,3 +9,13 @@ http.createServer(function(req, res) {
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
 }).listen(port); //the server object listens on port 8080
+
+
+*/
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function() {
+    console.log("Listening on " + server_ip_address + ", port " + server_port)
+});
